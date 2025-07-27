ae07b656e00837c05cbb481265008d0d82f9ac16
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 13:13:00 +0800

    style: budget page

---
+++ client/app/(panel)/budgets/page.tsx
@@ -0,0 +1,9 @@
+import PageTitle from "@/components/page-title";
+
+export default function BudgetsPage() {
+    return (
+        <div className="container mx-auto py-5">
+            <PageTitle title="Budgets" />
+        </div>
+    )
+}

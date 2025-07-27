2f97c5a70c8505f68cbce90c4b2b696850337994
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 11:46:01 +0800

    savings type

---
+++ client/types/savings.ts
@@ -0,0 +1,16 @@
+export type Savings = {
+    id: number
+    goal_name: string
+    target_amount: number
+    current_amount: number
+    start_date: string
+    target_date: string
+}
+
+export type SavingsCreate = {
+    goal_name: string
+    target_amount: number
+    current_amount: number
+    start_date: string
+    target_date: string
+}

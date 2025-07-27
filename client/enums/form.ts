6e36ff89a7197bd13c881a1172e9cdd657e85d23
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 11:39:28 +0800

    form dialog component types

---
+++ client/enums/form.ts
@@ -0,0 +1,11 @@
+export enum InputType {
+    INPUT = 'input',
+    SELECT = 'select',
+    TEXTAREA = 'textarea',
+}
+
+export enum FieldInputType {
+    TEXT = 'text',
+    NUMBER = 'number',
+    DATE = 'date',
+}

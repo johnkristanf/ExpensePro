ab02a1c117e0e74d3c8e4e4300578be6eaa121c5
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 11:41:55 +0800

    chores: removed unused type

--- client/app/(panel)/categories/columns.tsx
+++ client/app/(panel)/categories/columns.tsx
@@ -8,15 +8,7 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { MoreHorizontal } from 'lucide-react'
-
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type Categories = {
-    id: number
-    name: string
-    notes: string
-    created_at: string
-}
+import { Categories } from '@/types/categories'
 
 export const columns: ColumnDef<Categories>[] = [
     {

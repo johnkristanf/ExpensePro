5bc732063936e45bdf6484af4ffd3c1f60e2b8cb
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 11:40:42 +0800

    savings: model and migration

---
+++ server/database/migrations/2025_07_27_014844_create_savings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('savings', function (Blueprint $table) {
+            $table->id();
+            $table->string('goal_name');
+            $table->decimal('target_amount', 15, 2);
+            $table->decimal('current_amount', 15, 2);
+            $table->date('start_date');
+            $table->date('target_date');
+            // $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('savings');
+    }
+};

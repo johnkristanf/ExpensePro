4b783f0c728522dd9dc06cdfd90fd7bcc17b9703
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 11:44:42 +0800

    date and currency formatter

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function formatDateLocaleShort(date: string, locale = 'en-US') {
+    const d = new Date(date)
+    const month = d.toLocaleString(locale, { month: '2-digit' })
+    const day = d.toLocaleString(locale, { day: '2-digit' })
+    const year = String(d.getFullYear()).slice(-2) // last two digits
+
+    return `${month}/${day}/${year}`
+}
+
+export function formatAmount(amount: number): string {
+    const formatted = new Intl.NumberFormat('en-US').format(amount)
+    return formatted
 }

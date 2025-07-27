c628390ac5e28a5581af00de1dc2ee2a96b0817e
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 11:42:49 +0800

    style: margin bottom of pageTitle

--- client/components/page-title.tsx
+++ client/components/page-title.tsx
@@ -1,3 +1,3 @@
 export default function PageTitle({ title }: { title: string }) {
-    return <h1 className="text-3xl font-semibold">{title}</h1>
+    return <h1 className="text-3xl font-semibold mb-3">{title}</h1>
 }

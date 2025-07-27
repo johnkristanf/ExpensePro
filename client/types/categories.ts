64531c421aeccb86aab573e0f8e830c98d26eff3
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 11:44:20 +0800

    add categories fetch type

--- client/types/categories.ts
+++ client/types/categories.ts
@@ -1,3 +1,11 @@
+export type Categories = {
+    id: number
+    name: string
+    notes: string
+    created_at: string
+    updated_at: string
+}
+
 export type CategoriesCreate = {
     name: string
     notes: string

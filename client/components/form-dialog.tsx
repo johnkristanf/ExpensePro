93cf80e3ff4c98266854e830303f0439fbe274f3
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-07-27 21:11:22 +0800

    style: form dialog height and scrollable

--- client/components/form-dialog.tsx
+++ client/components/form-dialog.tsx
@@ -34,81 +34,82 @@ export default function FormDialog({ triggerLabel, title, fields, onSubmit }: Fo
     const { register, handleSubmit, reset, setValue, watch } = useForm()
 
     const handleFormSubmit: SubmitHandler<any> = (data) => {
-        onSubmit(data);
-        setOpen(false);
-        reset();
+        onSubmit(data)
+        setOpen(false)
+        reset()
     }
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
                 <Button>{triggerLabel}</Button>
             </DialogTrigger>
-            <DialogContent>
+            <DialogContent className="max-h-[80vh] overflow-y-auto">
                 <DialogHeader>
                     <DialogTitle>{title}</DialogTitle>
                 </DialogHeader>
                 <form onSubmit={handleSubmit(handleFormSubmit)} className="space-y-4">
-                    {fields && fields.map((field) => {
-                        const value = watch(field.name)
+                    {fields &&
+                        fields.map((field) => {
+                            const value = watch(field.name)
 
-                        switch (field.type) {
-                            case 'input':
-                                return (
-                                    <div key={field.name}>
-                                        <label className="block mb-2">{field.label}</label>
-                                        <Input
-                                            type={field.inputType && field.inputType}
-                                            {...register(field.name)}
-                                            placeholder={field.placeholder}
-                                            defaultValue={field.defaultValue}
-                                        />
-                                    </div>
-                                )
+                            switch (field.type) {
+                                case 'input':
+                                    return (
+                                        <div key={field.name}>
+                                            <label className="block mb-2">{field.label}</label>
+                                            <Input
+                                                type={field.inputType && field.inputType}
+                                                {...register(field.name)}
+                                                placeholder={field.placeholder}
+                                                defaultValue={field.defaultValue}
+                                            />
+                                        </div>
+                                    )
 
-                            case 'textarea':
-                                return (
-                                    <div key={field.name}>
-                                        <label className="block mb-1">{field.label}</label>
-                                        <Textarea
-                                            {...register(field.name)}
-                                            placeholder={field.placeholder}
-                                            defaultValue={field.defaultValue}
-                                        />
-                                    </div>
-                                )
-                            case 'select':
-                                return (
-                                    <div key={field.name}>
-                                        <label className="block mb-2">{field.label}</label>
-                                        <Select
-                                            defaultValue={field.defaultValue}
-                                            onValueChange={(val) => setValue(field.name, val)}
-                                            value={value}
-                                        >
-                                            <SelectTrigger className="w-full">
-                                                <SelectValue
-                                                    placeholder={
-                                                        field.placeholder || 'Select an option'
-                                                    }
-                                                />
-                                            </SelectTrigger>
-                                            <SelectContent>
-                                                {field.options?.map((option) => (
-                                                    <SelectItem
-                                                        key={option.value}
-                                                        value={option.value}
-                                                    >
-                                                        {option.label}
-                                                    </SelectItem>
-                                                ))}
-                                            </SelectContent>
-                                        </Select>
-                                    </div>
-                                )
-                            default:
-                                return null
-                        }
-                    })}
+                                case 'textarea':
+                                    return (
+                                        <div key={field.name}>
+                                            <label className="block mb-1">{field.label}</label>
+                                            <Textarea
+                                                {...register(field.name)}
+                                                placeholder={field.placeholder}
+                                                defaultValue={field.defaultValue}
+                                            />
+                                        </div>
+                                    )
+                                case 'select':
+                                    return (
+                                        <div key={field.name}>
+                                            <label className="block mb-2">{field.label}</label>
+                                            <Select
+                                                defaultValue={field.defaultValue}
+                                                onValueChange={(val) => setValue(field.name, val)}
+                                                value={value}
+                                            >
+                                                <SelectTrigger className="w-full">
+                                                    <SelectValue
+                                                        placeholder={
+                                                            field.placeholder || 'Select an option'
+                                                        }
+                                                    />
+                                                </SelectTrigger>
+                                                <SelectContent>
+                                                    {field.options?.map((option) => (
+                                                        <SelectItem
+                                                            key={option.value}
+                                                            value={option.value}
+                                                        >
+                                                            {option.label}
+                                                        </SelectItem>
+                                                    ))}
+                                                </SelectContent>
+                                            </Select>
+                                        </div>
+                                    )
+                                default:
+                                    return null
+                            }
+                        })}
                     <div className="flex justify-end gap-2 pt-4">
                         <Button variant="outline" type="button" onClick={() => setOpen(false)}>
                             Cancel

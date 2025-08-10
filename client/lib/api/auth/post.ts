f79af8edd15a9bc0b77b721dd1b2794edd24fdbf
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-08-10 09:33:46 +0800

    refactor: api route and web url on requests

--- client/lib/api/auth/post.ts
+++ client/lib/api/auth/post.ts
@@ -2,6 +2,6 @@ import { LoginCredentials } from '@/types/auth'
 import api from '../axios'
 
 export async function login(data: LoginCredentials) {
-    const response = await api.post(`/login`, data)
+    const response = await api.post(`${process.env.NEXT_PUBLIC_WEB_SERVER_URL}/login`, data)
     return response.data;
 }

a660f2cf9de5f28eee3032d79fe329094809c5c9
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-08-10 10:09:14 +0800

    refactor: add withCrsfToken header

--- client/lib/api/auth/post.ts
+++ client/lib/api/auth/post.ts
@@ -1,7 +1,7 @@
 import { LoginCredentials } from '@/types/auth'
-import api, { web } from '../axios'
+import { web } from '../axios'
 
 export async function login(data: LoginCredentials) {
     const response = await web.post(`/login`, data)
-    return response.data;
+    return await response.data;
 }
--- client/lib/api/axios.ts
+++ client/lib/api/axios.ts
@@ -14,6 +14,7 @@ const api = axios.create({
 export const web = axios.create({
     baseURL: process.env.NEXT_PUBLIC_WEB_SERVER_URL,
     withCredentials: true,
+    withXSRFToken: true,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',

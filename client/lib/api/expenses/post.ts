8440ba8ca153e6852d52f07e7b01af2f6eb3071d
John Kristan Torremocha <torremocha.johnkristan@gmail.com>
2025-08-10 16:30:20 +0800

    pass the expense payload

--- client/lib/api/expenses/post.ts
+++ client/lib/api/expenses/post.ts
@@ -2,6 +2,6 @@ import { ExpenseCreate } from '@/types/expenses'
 import api from '../axios'
 
 export async function createExpenses(data: ExpenseCreate) {
-    const response = await api.post(`/expenses`)
+    const response = await api.post(`/expenses`, data)
     return response.data;
 }

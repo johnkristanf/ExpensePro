9a77f63d574ffdf83a283bac329d0fdd07038b09
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-10-27 08:21:45 +0800

    package update

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,14 +1,11 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BudgetsController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ExpensesController;
 use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\SavingsController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/categories', [CategoriesController::class, 'index']);
